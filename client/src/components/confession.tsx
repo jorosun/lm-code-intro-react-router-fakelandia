265d385e54932f98f75dc770e3bf1098de12a05f
Jennifer Orosun <orosunj@gmail.com>
2023-08-14 01:37:24 +0100

    added confessButton and storeConfession to confession.tsx

--- client/src/components/confession.tsx
+++ client/src/components/confession.tsx
@@ -8,11 +8,49 @@ import {
 import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
 
+type ReasonForContact = MisdemeanourKind | "just-talk";
+
+interface Confession {
+  subject: string;
+  reason: ReasonForContact;
+  details: string;
+}
+
 const Confession = () => {
-  const [confessions, setConfessions] = useState<Confessions[]>([]);
+  const [confessions, setConfessions] = useState<Confession[]>([]);
   const effectCalled = useRef<boolean>(false);
 
+  const [subject, setSubject] = useState<string | null>(null);
+  const [reason, setReason] = useState<ReasonForContact | null>(null);
+  const [detail, setDetail] = useState<string | null>(null);
+
   const BASE_URL = "http://localhost:8080/api";
+
+  const confessButton = () => {
+    if (subject !== null || reason !== null || detail != null) {
+      return (
+        <div className="confess-button">
+          <button type="button" title="confess" value="Confess" />
+        </div>
+      );
+    }
+  };
+
+  const storeConfession = async () => {
+    try {
+      await axios({
+        method: "put",
+        url: `${BASE_URL}/confess`,
+        params: {
+          subject,
+          reason,
+          detail,
+        },
+      });
+    } catch (error) {
+      console.error("Error sending confession to server:", error);
+    }
+  };
 
   return (
     <section>
@@ -31,19 +69,15 @@ const Confession = () => {
             <label htmlFor="subject">Subject </label>
             <input type="text" id="subject" name="subject" required />
           </div>
-
           <div>
             <label htmlFor="reason"> Reason for Contact </label>
             <input type="text" id="reason" name="reason" required />
           </div>
-
           <fieldset>
             <div>
               <label htmlFor="details"> Details </label>
               <input type="text" id="details" name="details" required />
             </div>
-
-            <button type="button" title="confess" value="Confess" />
           </fieldset>
         </form>
       </div>
