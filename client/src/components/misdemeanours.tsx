79d55db66ef4206f3f5f38dc31cf9230f293cdf4
Jennifer Orosun <orosunj@gmail.com>
2023-08-16 17:41:42 +0100

    setMisdemeanours now working !!

--- client/src/components/misdemeanours.tsx
+++ client/src/components/misdemeanours.tsx
@@ -1,33 +1,30 @@
 import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
 import { Misdemeanour } from "../types/misdemeanours.types";
-// interface Misdemeanour {
-//   citizenId: number;
-//   misdemeanour: MisdemeanourKind;
-//   date: string;
-// }
 
 const MisdemeanoursPage: React.FC = () => {
   const [misdemeanours, setMisdemeanours] = useState<Misdemeanour[]>([]);
   const effectCalled = useRef<boolean>(false);
 
-  const BASE_URL = "http://localhost:8080/api/";
+  const BASE_URL = `http://localhost:8080/api/`;
   const amount = 2;
+  const table_URL = BASE_URL + `misdemeanours/${amount}`;
 
   useEffect(() => {
+    if (effectCalled.current) return;
+    effectCalled.current = true;
+
     const fetchMisdemeanours = async () => {
       try {
-        const response = await axios.get(BASE_URL + `misdemeanours/${amount}`);
-        setMisdemeanours(response.data.misdemeanour);
+        const response = await axios.get(table_URL);
+
+        setMisdemeanours(response.data.misdemeanours);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
     };
-
-    if (effectCalled.current) return;
     fetchMisdemeanours();
-    effectCalled.current = true;
-  }, []);
+  }, [misdemeanours, table_URL]);
 
   return (
     <section>
